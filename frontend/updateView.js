2eba07167fb2a887515fc462b9af7609cd846df5
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-12-02 17:29:51 +0100

    update file structure

--- frontend/updateView.js
+++
@@ -1,6 +0,0 @@
-const updateView = (newTitle, newContent) => {
-    let title = document.querySelector('#new-note-title').innerText = newTitle.full_name;
-    let content = document.querySelector('#new-note-content').innerText = newContent.description;
-}
-
-module.exports = updateView;
