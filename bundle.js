df3fd878298bcd604be6fa01de35ec4b21ef0701
Blessing <blessing@Blessings-Air.broadband>
2021-12-02 18:22:40 +0000

    set up project structure

--- bundle.js
+++ bundle.js
@@ -1,34 +1,4 @@
 (() => {
-  var __commonJS = (cb, mod) => function __require() {
-    return mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
-  };
-
-  // notes.js
-  var require_notes = __commonJS({
-    "notes.js"(exports, module) {
-      var getNote2 = (newTitle2, newContent2) => {
-        const data = { title: newTitle2, content: newContent2 };
-        fetch("http://localhost:3000/notes", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({ data })
-        }).then((response) => response.json()).then((data2) => {
-          console.log("New post:", data2);
-        });
-      };
-      module.exports = getNote2;
-    }
-  });
-
   // index.js
-  var getNotes = require_notes();
-  var getNote = require_notes();
-  var newTitle = document.querySelector("#new-note-title");
-  var newContent = document.querySelector("#new-note-content");
-  var button = document.querySelector("#submit-note");
-  button.addEventListener("click", () => {
-    getNote(newTitle.value, newContent.value);
-  });
+  console.log("Checking bundler");
 })();
--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const getNotes = require('./notes');
-const getNote = require('./notes');
-
-const newTitle = document.querySelector('#new-note-title')
-const newContent = document.querySelector('#new-note-content')
-
-const button = document.querySelector('#submit-note');
-
-button.addEventListener("click", () => {
-    getNote(newTitle.value, newContent.value);
-});
-
+console.log('Checking bundler');
