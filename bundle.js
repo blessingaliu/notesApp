e726c6a3b9e51f52681b00ca435acc55a9d03b6c
Blessing <blessing@Blessings-Air.broadband>
2021-12-02 20:08:14 +0000

    added some bootstrap

--- bundle.js
+++ bundle.js
@@ -7,13 +7,13 @@
   var require_makeNewNote = __commonJS({
     "makeNewNote.js"(exports, module) {
       var makeNewNote2 = (title2, content2, callback) => {
-        const data = { "title": `${title2}`, "content": `${content2}` };
+        const data = { title: title2, content: content2 };
         fetch("http://localhost:3000/notes", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
           },
-          body: JSON.stringify(data)
+          body: JSON.stringify({ "title": `${title2}`, "content": `${content2}` })
         }).then((response) => response.json()).then((data2) => {
           console.log("New post:", data2);
         }).catch((error) => {
@@ -24,8 +24,19 @@
     }
   });
 
+  // showNotes.js
+  var require_showNotes = __commonJS({
+    "showNotes.js"(exports, module) {
+      var showNotes2 = (callback) => {
+        fetch("http://localhost:3000/notes").then((response) => response.json()).then((data) => callback(data));
+      };
+      module.exports = showNotes2;
+    }
+  });
+
   // index.js
   var makeNewNote = require_makeNewNote();
+  var showNotes = require_showNotes();
   var button = document.querySelector("#click-btn");
   var title = document.querySelector("#note-title");
   var content = document.querySelector("#note-content");
